ec0ea572e8a5e2e07c2133d1343084789ef278ed
Adam Liddell <git@aliddell.com>
2019-06-26 17:35:35 +0000

    Remove unused templates

--- tools/rulegen/common.go
+++ tools/rulegen/common.go
@@ -4,59 +4,6 @@ package main
 var commonLangFlags = []*Flag{}
 
 
-var protoCompileAttrs = []*Attr{
-	&Attr{
-		Name:      "deps",
-		Type:      "list<ProtoInfo>",
-		Default:   "[]",
-		Doc:       "List of labels that provide a `ProtoInfo` (such as `native.proto_library`)",
-		Mandatory: true,
-	},
-	&Attr{
-		Name:      "plugins",
-		Type:      "list<ProtoPluginInfo>",
-		Default:   "[]",
-		Doc:       "List of labels that provide a `ProtoPluginInfo`",
-		Mandatory: false,
-	},
-	&Attr{
-		Name:      "plugin_options",
-		Type:      "list<string>",
-		Default:   "[]",
-		Doc:       "List of additional 'global' plugin options (applies to all plugins). To apply plugin specific options, use the `options` attribute on `proto_plugin`",
-		Mandatory: false,
-	},
-	&Attr{
-		Name:      "outputs",
-		Type:      "list<generated file>",
-		Default:   "[]",
-		Doc:       "List of additional expected generated file outputs",
-		Mandatory: false,
-	},
-	&Attr{
-		Name:      "verbose",
-		Type:      "int",
-		Default:   "0",
-		Doc:       "The verbosity level. Supported values and results are 1: *show command*, 2: *show command and sandbox after running protoc*, 3: *show command and sandbox before and after running protoc*, 4. *show env, command, expected outputs and sandbox before and after running protoc*",
-		Mandatory: false,
-	},
-	&Attr{
-		Name:      "include_imports",
-		Type:      "bool",
-		Default:   "True",
-		Doc:       "Pass the --include_imports argument to the protoc_plugin",
-		Mandatory: false,
-	},
-	&Attr{
-		Name:      "include_source_info",
-		Type:      "bool",
-		Default:   "True",
-		Doc:       "Pass the --include_source_info argument to the protoc_plugin",
-		Mandatory: false,
-	},
-}
-
-
 var aspectProtoCompileAttrs = []*Attr{
 	&Attr{
 		Name:      "deps",
@@ -73,16 +20,6 @@ var aspectProtoCompileAttrs = []*Attr{
 		Mandatory: false,
 	},
 }
-
-
-var compileRuleTemplate = mustTemplate(`load("//:compile.bzl", "proto_compile")
-
-def {{ .Rule.Name }}(**kwargs):
-    # Append the {{ .Lang.Name }} plugins and call generic compile
-    kwargs["plugins"] = kwargs.get("plugins", []) + [{{ range .Rule.Plugins }}
-        Label("{{ . }}"),{{ end }}
-    ]
-    proto_compile(**kwargs)`)
 
 
 var aspectRuleTemplate = mustTemplate(`load("//:plugin.bzl", "ProtoPluginInfo")
