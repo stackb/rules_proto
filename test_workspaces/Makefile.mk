bfa93e64cb564b2857f6a3673172516ffed4e28a
Adam Liddell <git@aliddell.com>
2019-06-27 12:10:45 +0000

    Add test workspace that uses a generated proto file

--- test_workspaces/Makefile.mk
+++ test_workspaces/Makefile.mk
@@ -3,6 +3,11 @@ test_workspace_exclusions:
 	bazel test --disk_cache=../bazel-disk-cache --test_output=errors //... ; \
 	bazel shutdown
 
+test_workspace_generated_proto:
+	cd test_workspaces/generated_proto; \
+	bazel test --disk_cache=../bazel-disk-cache --test_output=errors //... ; \
+	bazel shutdown
+
 test_workspace_objc_capitalisation:
 	cd test_workspaces/objc_capitalisation; \
 	bazel test --disk_cache=../bazel-disk-cache --test_output=errors //... ; \
@@ -33,4 +38,4 @@ test_workspace_strip_import_prefix:
 	bazel test --disk_cache=../bazel-disk-cache --test_output=errors //... ; \
 	bazel shutdown
 
-all_test_workspaces: test_workspace_exclusions test_workspace_objc_capitalisation test_workspace_proto_source_root test_workspace_python_dashes test_workspace_python_deps test_workspace_shared_proto test_workspace_strip_import_prefix
+all_test_workspaces: test_workspace_exclusions test_workspace_generated_proto test_workspace_objc_capitalisation test_workspace_proto_source_root test_workspace_python_dashes test_workspace_python_deps test_workspace_shared_proto test_workspace_strip_import_prefix
---
+++ test_workspaces/generated_proto/main.py
@@ -0,0 +1,2 @@
+# Attempt to import proto file, this should succeed
+import demo_pb2
