f84ff3de2518bcff7386420cf2a971257ee66eeb
Adam Liddell <git@aliddell.com>
2019-07-07 22:00:16 +0000

    Test re-enabling rust rules on windows

--- tools/rulegen/rust.go
+++ tools/rulegen/rust.go
@@ -78,7 +78,7 @@ func makeRust() *Language {
 		Dir:  "rust",
 		Name: "rust",
 		Flags: commonLangFlags,
-		BazelCIExcludePlatforms: []string{"windows"}, // CI has no rust toolchain for windows
+		//BazelCIExcludePlatforms: []string{"windows"}, // CI has no rust toolchain for windows
 		Rules: []*Rule{
 			&Rule{
 				Name:             "rust_proto_compile",
