3f840b048e9b1b1ead75176bad9cf6ae99b3aa8f
Paul Johnston <paul.johnston02@sap.com>
2021-09-18 14:50:14 -0600

    No need to return options, it will be done automatically

--- pkg/plugin/scalapb/scalapb/protoc_gen_scala.go
+++ pkg/plugin/scalapb/scalapb/protoc_gen_scala.go
@@ -26,6 +26,5 @@ func (p *ProtocGenScalaPlugin) Configure(ctx *protoc.PluginContext) *protoc.Plug
 	return &protoc.PluginConfiguration{
 		Label:   label.New("build_stack_rules_proto", "plugin/scalapb/scalapb", "protoc-gen-scala"),
 		Outputs: []string{path.Join(ctx.Rel, ctx.ProtoLibrary.BaseName()+"_scala.srcjar")},
-		Options: ctx.PluginConfig.GetOptions(),
 	}
 }
