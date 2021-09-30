2b3727d14eddaffd390ae73bf2aed1b074a0600a
Paul Johnston <paul.johnston@robinhood.com>
2021-09-29 20:05:44 -0600

    Commit package name changes

--- pkg/plugin/builtin_test/doc.go
+++ pkg/plugin/builtin_test/doc.go
@@ -1,2 +1,2 @@
 // plugin builtin_test is for testing of the pkg/builtin plugins.
-package builtin_test
+package builtin
--- pkg/plugin/grpc/grpc_test/grpc_test.go
+++ pkg/plugin/grpc/grpc_test/grpc_test.go
@@ -1,4 +1,4 @@
-package grpc_test
+package grpc_test_test
 
 import (
 	"testing"
