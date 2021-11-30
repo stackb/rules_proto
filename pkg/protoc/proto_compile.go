5c2dfa60af8084f0bf07a1a9890458d9536c1476
Paul Johnston <paul.johnston@robinhood.com>
2021-11-29 21:58:06 -0700

    Use same rule impl

--- pkg/protoc/proto_compile.go
+++ pkg/protoc/proto_compile.go
@@ -57,27 +57,45 @@ func (s *protoCompile) LoadInfo() rule.LoadInfo {
 
 // ProvideRule implements part of the LanguageRule interface.
 func (s *protoCompile) ProvideRule(cfg *LanguageRuleConfig, config *ProtocConfiguration) RuleProvider {
-	return &protoCompileRule{config}
+	return &protoCompileRule{
+		kind:            "proto_compile",
+		nameSuffix:      "compile",
+		outputsAttrName: "outputs",
+		config:          config,
+		ruleConfig:      cfg,
+	}
 }
 
 // protoCompile implements RuleProvider for the 'proto_compile' rule.
 type protoCompileRule struct {
-	config *ProtocConfiguration
+	kind            string
+	nameSuffix      string
+	outputsAttrName string
+	config          *ProtocConfiguration
+	ruleConfig      *LanguageRuleConfig
 }
 
 // Kind implements part of the ruleProvider interface.
 func (s *protoCompileRule) Kind() string {
-	return "proto_compile"
+	return s.kind
 }
 
 // Name implements part of the ruleProvider interface.
 func (s *protoCompileRule) Name() string {
-	return fmt.Sprintf("%s_%s_compile", s.config.Library.BaseName(), s.config.Prefix)
+	return fmt.Sprintf("%s_%s_%s", s.config.Library.BaseName(), s.config.Prefix, s.nameSuffix)
 }
 
 // Visibility provides visibility labels.
 func (s *protoCompileRule) Visibility() []string {
-	return nil // TODO: visibility feature?
+	visibility := make([]string, 0)
+	for k, want := range s.ruleConfig.Visibility {
+		if !want {
+			continue
+		}
+		visibility = append(visibility, k)
+	}
+	sort.Strings(visibility)
+	return visibility
 }
 
 // Rule implements part of the ruleProvider interface.
@@ -87,7 +105,7 @@ func (s *protoCompileRule) Rule(otherGen ...*rule.Rule) *rule.Rule {
 	outputs := s.config.Outputs
 	sort.Strings(outputs)
 
-	newRule.SetAttr("outputs", outputs)
+	newRule.SetAttr(s.outputsAttrName, outputs)
 	newRule.SetAttr("plugins", GetPluginLabels(s.config.Plugins))
 	newRule.SetAttr("proto", s.config.Library.Name())
 
