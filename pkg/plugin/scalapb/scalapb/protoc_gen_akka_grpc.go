780b1b725d2edd3640d8ce98baa58952c6f22460
Paul Johnston <pcj@stack.build>
2022-02-10 12:46:46 -0700

    Add initial protoc-gen-akka-grpc

---
+++ pkg/plugin/scalapb/scalapb/protoc_gen_akka_grpc.go
@@ -0,0 +1,40 @@
+package scalapb
+
+import (
+	"path"
+
+	"github.com/bazelbuild/bazel-gazelle/label"
+	"github.com/stackb/rules_proto/pkg/protoc"
+)
+
+const AkkaGrpcPluginName = "akka:akka-grpc:protoc-gen-akka-grpc"
+
+func init() {
+	protoc.Plugins().MustRegisterPlugin(&protocGenAkkaGrpcPlugin{})
+}
+
+// protocGenAkkaGrpcPlugin implements Plugin for the akka-grpc plugin.
+type protocGenAkkaGrpcPlugin struct{}
+
+// Name implements part of the Plugin interface.
+func (p *protocGenAkkaGrpcPlugin) Name() string {
+	return AkkaGrpcPluginName
+}
+
+// Configure implements part of the Plugin interface.
+func (p *protocGenAkkaGrpcPlugin) Configure(ctx *protoc.PluginContext) *protoc.PluginConfiguration {
+	if !protoc.HasServices(ctx.ProtoLibrary.Files()...) {
+		return nil
+	}
+
+	srcjar := ctx.ProtoLibrary.BaseName() + "_akka.srcjar"
+	if ctx.Rel != "" {
+		srcjar = path.Join(ctx.Rel, srcjar)
+	}
+
+	return &protoc.PluginConfiguration{
+		Label:   label.New("build_stack_rules_proto", "plugin/akka/akka-grpc", "protoc-gen-akka-grpc"),
+		Outputs: []string{srcjar},
+		Options: ctx.PluginConfig.GetOptions(),
+	}
+}
