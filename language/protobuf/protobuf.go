299998fd537dbc7f9fb581c6e8a0da541e6d2c8d
Alex Eagle <alex@aspect.dev>
2023-08-15 19:00:12 -0700

    WIP: start on gazelle extension

--- language/protobuf/protobuf.go
+++ language/protobuf/protobuf.go
@@ -16,6 +16,7 @@ import (
 	_ "github.com/stackb/rules_proto/pkg/plugin/grpcecosystem/grpcgateway"
 	_ "github.com/stackb/rules_proto/pkg/plugin/scalapb/scalapb"
 	_ "github.com/stackb/rules_proto/pkg/plugin/stackb/grpc_js"
+	_ "github.com/stackb/rules_proto/pkg/plugin/bufbuild"
 	_ "github.com/stackb/rules_proto/pkg/plugin/stephenh/ts-proto"
 	_ "github.com/stackb/rules_proto/pkg/rule/rules_cc"
 	_ "github.com/stackb/rules_proto/pkg/rule/rules_closure"
---
+++ pkg/plugin/bufbuild/connect_plugin.go
@@ -0,0 +1,109 @@
+package bufbuild
+
+import (
+	"flag"
+	"log"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/bazelbuild/bazel-gazelle/label"
+	"github.com/stackb/rules_proto/pkg/protoc"
+)
+
+func init() {
+	protoc.Plugins().MustRegisterPlugin(&ConnectProto{})
+}
+
+// ConnectProto implements Plugin for the bufbuild/connect-es plugin.
+type ConnectProto struct{}
+
+// Name implements part of the Plugin interface.
+func (p *ConnectProto) Name() string {
+	return "bufbuild:connect-es"
+}
+
+// Configure implements part of the Plugin interface.
+func (p *ConnectProto) Configure(ctx *protoc.PluginContext) *protoc.PluginConfiguration {
+	flags := parseConnectProtoOptions(p.Name(), ctx.PluginConfig.GetFlags())
+	imports := make(map[string]bool)
+	for _, file := range ctx.ProtoLibrary.Files() {
+		for _, imp := range file.Imports() {
+			imports[imp.Filename] = true
+		}
+	}
+	var emitImportedFiles bool
+	var options []string
+	for _, option := range ctx.PluginConfig.GetOptions() {
+		// options may be configured to include many "M" options, but only
+		// include the relevant ones to avoid BUILD file clutter.
+		if strings.HasPrefix(option, "M") {
+			keyVal := option[len("M"):]
+			parts := strings.SplitN(keyVal, "=", 2)
+			filename := parts[0]
+			if !imports[filename] {
+				continue
+			}
+		}
+		// if option == "emitImportedFiles=true" {
+		// 	emitImportedFiles = true
+		// }
+		options = append(options, option)
+	}
+
+	tsFiles := make([]string, 0)
+	for _, file := range ctx.ProtoLibrary.Files() {
+		if emitImportedFiles {
+			for _, imp := range file.Imports() {
+				if !strings.HasPrefix(imp.Filename, "google/protobuf") {
+					continue
+				}
+				tsFiles = append(tsFiles, strings.TrimSuffix(imp.Filename, ".proto")+".ts")
+			}
+		}
+
+		tsFile := file.Name + ".ts"
+		if flags.excludeOutput[filepath.Base(tsFile)] {
+			continue
+		}
+		if ctx.Rel != "" {
+			tsFile = path.Join(ctx.Rel, tsFile)
+		}
+		tsFiles = append(tsFiles, tsFile)
+	}
+
+	pc := &protoc.PluginConfiguration{
+		Label:   label.New("build_stack_rules_proto", "plugin/bufbuild", "es"),
+		Outputs: protoc.DeduplicateAndSort(tsFiles),
+		Options: protoc.DeduplicateAndSort(options),
+	}
+	if len(pc.Outputs) == 0 {
+		pc.Outputs = nil
+	}
+	return pc
+}
+
+// ConnectProtoOptions represents the parsed flag configuration for the
+// ConnectProto implementation.
+type ConnectProtoOptions struct {
+	excludeOutput map[string]bool
+}
+
+func parseConnectProtoOptions(kindName string, args []string) *ConnectProtoOptions {
+	flags := flag.NewFlagSet(kindName, flag.ExitOnError)
+
+	var excludeOutput string
+	flags.StringVar(&excludeOutput, "exclude_output", "", "--exclude_output=foo.ts suppresses the file 'foo.ts' from the output list")
+
+	if err := flags.Parse(args); err != nil {
+		log.Fatalf("failed to parse flags for %q: %v", kindName, err)
+	}
+	config := &ConnectProtoOptions{
+		excludeOutput: make(map[string]bool),
+	}
+	for _, value := range strings.Split(excludeOutput, ",") {
+		config.excludeOutput[value] = true
+	}
+
+	return config
+}
