52fcb49627defce77f8a18ea837ed1fb3b165951
Paul Cody Johnston <pcj@stack.build>
2023-11-13 11:26:20 -0700

    Allow starlark rule to return nil if it does not want to generate a rule (#349)

--- pkg/protoc/starlark_rule_test.go
+++ pkg/protoc/starlark_rule_test.go
@@ -91,6 +91,40 @@ protoc.Rule(
 				`ProtocConfiguration(imports = [], language_config = LanguageConfig(enabled = False, name = "", plugins = {}, protoc = "", rules = {}), mappings = {}, outputs = [], package_config = PackageConfig(config = Config(repo_name = "", repo_root = "", work_dir = "")), plugins = [], prefix = "", proto_library = ProtoLibrary(base_name = "", deps = [], files = [], imports = [], name = "", srcs = [], strip_import_prefix = ""), rel = "")` +
 				"\n",
 		},
+		"may-return-none": {
+			code: `
+def make_py_library_rule(self):
+	return None
+
+def provide_rule(rctx, pctx):
+	print(rctx)
+	print("---")
+	print(pctx)
+	return struct(
+		name = rctx.name,
+		kind = "py_library",
+		rule = make_py_library_rule,
+	)
+
+protoc.Rule(
+	name = "test",
+	load_info = lambda: None,
+	kind_info = lambda: None,
+	provide_rule = provide_rule,
+)
+			`,
+			rc: &LanguageRuleConfig{
+				Config:  &config.Config{},
+				Name:    "test",
+				Options: map[string]bool{"grpc": true},
+			},
+			pc:      &ProtocConfiguration{},
+			wantErr: nil,
+			wantPrinted: `LanguageRuleConfig(attrs = {}, config = Config(repo_name = "", repo_root = "", work_dir = ""), deps = [], enabled = False, implementation = "", name = "test", options = ["grpc"], visibility = [])` +
+				"\n---\n" +
+				`ProtocConfiguration(imports = [], language_config = LanguageConfig(enabled = False, name = "", plugins = {}, protoc = "", rules = {}), mappings = {}, outputs = [], package_config = PackageConfig(config = Config(repo_name = "", repo_root = "", work_dir = "")), plugins = [], prefix = "", proto_library = ProtoLibrary(base_name = "", deps = [], files = [], imports = [], name = "", srcs = [], strip_import_prefix = ""), rel = "")` +
+				"\n",
+		},
 	} {
 		t.Run(name, func(t *testing.T) {
 			var err error
