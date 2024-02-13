91034721570de500d112b4d02915859736ca9092
Paul Johnston <pcj@stack.build>
2024-02-12 22:43:44 -0700

    Update release.yaml

--- .github/workflows/workspace_snippet.sh
+++ .github/workflows/workspace_snippet.sh
@@ -4,30 +4,19 @@ set -eufo pipefail
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
-PREFIX="rules_jvm-${TAG:1}"
+PREFIX="rules_proto-${TAG:1}"
 SHA=$(git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip | shasum -a 256 | awk '{print $1}')
 
 cat << EOF
 WORKSPACE snippet:
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
 http_archive(
-    name = "contrib_rules_jvm",
+    name = "build_stack_rules_proto",
     sha256 = "${SHA}",
     strip_prefix = "${PREFIX}",
-    url = "https://github.com/bazel-contrib/rules_jvm/archive/refs/tags/${TAG}.tar.gz",
+    url = "https://github.com/stackb/rules_proto/archive/refs/tags/${TAG}.tar.gz",
 )
-
-# Fetches the contrib_rules_jvm dependencies.
-# If you want to have a different version of some dependency,
-# you should fetch it *before* calling this.
-load("@contrib_rules_jvm//:repositories.bzl", "contrib_rules_jvm_deps")
-
-contrib_rules_jvm_deps()
-
-# Now ensure that the downloaded deps are properly configured
-load("@contrib_rules_jvm//:setup.bzl", "contrib_rules_jvm_setup")
-
-contrib_rules_jvm_setup()
 \`\`\`
 EOF
