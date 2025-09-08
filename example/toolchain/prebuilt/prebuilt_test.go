7a8243cd14c78cba9397b0ede41c5fcb70d7a333
Paul Johnston <pcj@stack.build>
2025-09-08 08:37:50 -0600

    fix: remove obsolete go_bazel_test

--- example/toolchain/prebuilt/prebuilt_test.go
+++ example/toolchain/prebuilt/prebuilt_test.go
@@ -16,7 +16,8 @@ func TestMain(m *testing.M) {
 # TODO(pcj): why do we need a bazel_dep for rules_go?  It would seem to be setup by go_bazel_test already
 bazel_dep(name = "rules_go", version = "0.57.0", repo_name = "io_bazel_rules_go")
 go_sdk = use_extension("@io_bazel_rules_go//go:extensions.bzl", "go_sdk")
-go_sdk.download(version = "1.23.1")`,
+go_sdk.download(version = "1.23.1")
+`,
 	})
 }
 
