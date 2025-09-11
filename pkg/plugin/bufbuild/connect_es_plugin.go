bca0548e4b86e89ae3b4a942bbabef5d28e9952f
Paul Johnston <pcj@stack.build>
2025-09-11 12:42:19 -0600

    lint and visibility fixes

--- pkg/plugin/bufbuild/connect_es_plugin.go
+++ pkg/plugin/bufbuild/connect_es_plugin.go
@@ -26,12 +26,7 @@ func (p *ConnectEsProto) Name() string {
 // Configure implements part of the Plugin interface.
 func (p *ConnectEsProto) Configure(ctx *protoc.PluginContext) *protoc.PluginConfiguration {
 	flags := parseConnectEsProtoOptions(p.Name(), ctx.PluginConfig.GetFlags())
-	imports := make(map[string]bool)
-	for _, file := range ctx.ProtoLibrary.Files() {
-		for _, imp := range file.Imports() {
-			imports[imp.Filename] = true
-		}
-	}
+
 	// TODO: get target option from directive
 	var options = []string{"keep_empty_files=true", "target=ts"}
 	tsFiles := make([]string, 0)
@@ -55,16 +50,17 @@ func (p *ConnectEsProto) Configure(ctx *protoc.PluginContext) *protoc.PluginConf
 	if len(pc.Outputs) == 0 {
 		pc.Outputs = nil
 	}
+
 	return pc
 }
 
-// ConnectEsProtoOptions represents the parsed flag configuration for the
+// connectEsProtoOptions represents the parsed flag configuration for the
 // ConnectEsProto implementation.
-type ConnectEsProtoOptions struct {
+type connectEsProtoOptions struct {
 	excludeOutput map[string]bool
 }
 
-func parseConnectEsProtoOptions(kindName string, args []string) *ConnectEsProtoOptions {
+func parseConnectEsProtoOptions(kindName string, args []string) *connectEsProtoOptions {
 	flags := flag.NewFlagSet(kindName, flag.ExitOnError)
 
 	var excludeOutput string
@@ -73,7 +69,7 @@ func parseConnectEsProtoOptions(kindName string, args []string) *ConnectEsProtoO
 	if err := flags.Parse(args); err != nil {
 		log.Fatalf("failed to parse flags for %q: %v", kindName, err)
 	}
-	config := &ConnectEsProtoOptions{
+	config := &connectEsProtoOptions{
 		excludeOutput: make(map[string]bool),
 	}
 	for _, value := range strings.Split(excludeOutput, ",") {
